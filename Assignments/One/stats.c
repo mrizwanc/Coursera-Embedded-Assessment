348accdf0d9a03f2dc7e28d1ef32d78224674f66
Md. Rizwan Chowdhury <48764917+mrizwanc@users.noreply.github.com>
2024-05-26 12:52:44 +0600

    solved stats.c

--- Assignments/One/stats.c
+++ Assignments/One/stats.c
@@ -1,7 +1,7 @@
 /******************************************************************************
  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
  *
- * Redistribution, modification or use of this software in source or binary
+ * Redistribution, modification, or use of this software in source or binary
  * forms is permitted as long as the files maintain this copyright. Users are 
  * permitted to modify this and use it to learn about the field of embedded
  * software. Alex Fosdick and the University of Colorado are not liable for any
@@ -9,13 +9,13 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file <stats.c> 
+ * @brief <The Implementation File Of The c1m1 Assessment>
  *
- * <Add Extended Description Here>
+ * <This file includes all the required functions implementations in the c1m1 assessment>
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author <Md. Rizwan Chowdhury>
+ * @date <18/05/2024>
  *
  */
 
@@ -36,43 +36,109 @@ void main() {
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
   /* Other Variable Declarations Go Here */
+  
+  unsigned char minimum = 0;
+  unsigned char maximum = 0;
+  float mean = 0;
+  unsigned char median = 0;
+  
   /* Statistics and Printing Functions Go Here */
+
+  printf("Array Unsorted: \n");
+  print_array(&test, SIZE);
+  median = find_median(&test, SIZE);
+  mean = find_mean(&test, SIZE);
+  maximum = find_maximum(&test, SIZE);
+  minimum = find_minimum(&test, SIZE);
+  print_statistics(minimum, maximum, mean, median);
+  sort_array(&test, SIZE);
+  printf("Array Sorted: \n");
+  print_array(&test, SIZE);
 
 }
 
 /* Add other Implementation File Code Here */
 
-void print_statistics()
+void print_statistics(unsigned char minimum, unsigned char maximum, float mean, unsigned char median)
 {
-
+  printf("Minimum: %d \n", minimum);
+  printf("Maximum: %d \n", maximum);
+  printf("Mean: %f \n", mean);
+  printf("Median: %d \n", median);
 }
 
-void print_array()
-{
 
+void print_array(unsigned char *array, unsigned int sz)
+{
+  for (int i=0; i<sz; i++)
+  {
+    printf("%d, ", array[i]);
+  }
+  printf("\n");
 }
 
-void find_median()
-{
 
+unsigned char find_median(unsigned char *array, unsigned int sz)
+{
+  unsigned char median = 0;
+  median = *(array + (sz / 2) -1);
+  return median;
 }
 
-void find_mean()
-{
 
+float find_mean (unsigned char *array, unsigned int sz)
+{
+  unsigned int accumulator = 0; //storing accumulator value in the mean finding process
+  float mean = 0;
+  for (int i=0; i<sz; i++)
+  {
+    accumulator += array[i];
+  }
+  printf("acc = %d \n", accumulator);
+  mean = accumulator / ((float) counter); //typecasting for accurate calculation.
+  return mean;
 }
 
-void find_maximum()
+unsigned char find_maximum (unsigned char *array, unsigned int sz)
 {
-
+  unsigned char max = array[0];
+  for (int i=1; i<sz; i++)
+  {
+    if ( array[i] > max)
+    {
+      max = array[i];
+    }
+  }
+  return max;
 }
 
-void find_minimum()
+unsigned char find_minimum (unsigned char *array, unsigned int sz)
 {
-
+  unsigned char min = array[0];
+  for (int i=1; i<sz; i++)
+  {
+    if ( array[i] < min )
+    {
+      min = array[i];
+    }
+  }
+  return min;
 }
 
-void sort_array()
+void sort_array (unsigned char *a, unsigned int sz)
 {
+  int i,j;
+  for (i = 0; i < sz; i++) 
+  {
+    for (j = i + 1; j < sz; j++)
+    {
+      if (a[i] < a[j])
+      {
+        t = a[i]; 
+        a[i] = a[j]; 
+        a[j] = t; 
+      } 
+    } 
+ }
 
 }
